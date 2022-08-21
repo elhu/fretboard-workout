2cb0b251fd5e1424a445b7efce828c4f6a35091a
Vincent Boisard <boisard.v@gmail.com>
2022-08-21 15:47:09 +0200

    Basic guessing game for pentatonic shape and root

--- src/App.js
+++ src/App.js
@@ -1,53 +1,27 @@
 import "./App.css";
-import { useEffect, useState } from "react";
-import { FretboardComponent } from "./FretboardComponent";
-import { DropDown } from "./DropDown";
-import { minorPentatonicShapesFor, notes } from "./utils";
+import { PentatonicShapeToNameContainer } from "./exercises/PentatonicShapeToName";
 
 function App() {
-  let [dots, setDots] = useState([]);
-  let [root, setRoot] = useState(undefined);
-  let [shape, setShape] = useState(undefined);
+  // useEffect(() => {
+  //   if (root !== undefined && shape !== undefined) {
+  //     setDots(minorPentatonicShapesFor({ root, shape }).flat());
+  //   }
+  // }, [shape, root]);
 
-  useEffect(() => {
-    if (root !== undefined && shape !== undefined) {
-      setDots(minorPentatonicShapesFor({ root, shape }).flat());
-    }
-  }, [shape, root]);
-
-  const onFretboardClick = ({ fret, string }) => {
-    const idx = dots.findIndex((x) => x.fret === fret && x.string === string);
-    if (idx == -1) {
-      dots = dots.concat([{ fret: fret, string: string }]);
-      setDots(dots);
-    } else {
-      dots = dots.slice(0, idx).concat(dots.slice(idx + 1, dots.length));
-      setDots(dots);
-    }
-  };
+  // const onFretboardClick = ({ fret, string }) => {
+  //   const idx = dots.findIndex((x) => x.fret === fret && x.string === string);
+  //   if (idx == -1) {
+  //     dots = dots.concat([{ fret: fret, string: string }]);
+  //     setDots(dots);
+  //   } else {
+  //     dots = dots.slice(0, idx).concat(dots.slice(idx + 1, dots.length));
+  //     setDots(dots);
+  //   }
+  // };
 
   return (
     <div className="App">
-      <DropDown
-        options={notes.map((n) => {
-          return { value: n, text: n };
-        })}
-        defaultText="Select a root"
-        onChange={(e) => setRoot(e.target.value)}
-      />
-      <DropDown
-        options={Array.from({ length: 5 }, (_, i) => i + 1).map((n) => {
-          return { value: `${n}`, text: `${n}` };
-        })}
-        defaultText="Select a pentatonic shape"
-        onChange={(e) => setShape(e.target.value)}
-      />
-      <FretboardComponent
-        dots={dots}
-        showNotes={true}
-        onClick={onFretboardClick}
-        editable={true}
-      />
+      <PentatonicShapeToNameContainer />
     </div>
   );
 }
---
+++ src/exercises/PentatonicShapeToName.js
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import { FretboardComponent } from "../FretboardComponent";
+import { DropDown } from "../DropDown";
+import { minorPentatonicShapesFor, notes } from "../utils";
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function randomRoot() {
+  return notes[getRandomInt(notes.length)];
+}
+
+function randomShape() {
+  return getRandomInt(5) + 1;
+}
+
+function PentatonicShapeToNameContainer() {
+  let [root, setRoot] = useState(randomRoot());
+  let [shape, setShape] = useState(randomShape());
+
+  const onNextClick = () => {
+    setRoot(randomRoot());
+    setShape(randomShape());
+  };
+
+  return (
+    <PentatonicShapeToName
+      root={root}
+      shape={shape}
+      onNextClick={onNextClick}
+    />
+  );
+}
+
+function PentatonicShapeToName(props) {
+  let { root, shape, onNextClick } = props;
+
+  let [showNotes, setShowNotes] = useState(false);
+  let [selectedRoot, setSelectedRoot] = useState();
+  let [selectedShape, setSelectedShape] = useState();
+  let [winState, setWinState] = useState();
+  let shapes = minorPentatonicShapesFor({ root, shape });
+  let dots = shapes[getRandomInt(shapes.length)];
+
+  const onSubmit = () =>
+    setWinState(
+      selectedRoot === root && String(selectedShape) === String(shape)
+    );
+
+  let winBanner;
+  if (winState === false) {
+    winBanner = <p>Try again!</p>;
+  }
+  if (winState === true) {
+    winBanner = (
+      <div>
+        <p>Congrats, you guessed right!</p>
+        <button onClick={onNextClick}>Next</button>
+      </div>
+    );
+  }
+  return (
+    <div className="App">
+      <DropDown
+        options={notes.map((n) => {
+          return { value: n, text: n };
+        })}
+        onChange={(e) => setSelectedRoot(e.target.value)}
+        defaultText="Select a root"
+      />
+      <DropDown
+        options={Array.from({ length: 5 }, (_, i) => i + 1).map((n) => {
+          return { value: n, text: `${n}` };
+        })}
+        onChange={(e) => setSelectedShape(e.target.value)}
+        defaultText="Select a pentatonic shape"
+      />
+      <button onClick={onSubmit}>Submit</button>
+      {winBanner}
+      <FretboardComponent dots={dots} showNotes={showNotes} editable={false} />
+    </div>
+  );
+}
+
+export { PentatonicShapeToNameContainer };
