f9dff36549e33cfc405d9c8d1ed1bce07da89d98
Vincent Boisard <boisard.v@gmail.com>
2022-08-22 08:13:12 +0200

    Dummy side menu

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import { PentatonicNameToShapeContainer } from "./exercises/PentatonicNameToShape";
 import { PentatonicShapeToNameContainer } from "./exercises/PentatonicShapeToName";
 
 function App() {
-  // useEffect(() => {
-  //   if (root !== undefined && shape !== undefined) {
-  //     setDots(minorPentatonicShapesFor({ root, shape }).flat());
-  //   }
-  // }, [shape, root]);
-
-  // const onFretboardClick = ({ fret, string }) => {
-  //   const idx = dots.findIndex((x) => x.fret === fret && x.string === string);
-  //   if (idx == -1) {
-  //     dots = dots.concat([{ fret: fret, string: string }]);
-  //     setDots(dots);
-  //   } else {
-  //     dots = dots.slice(0, idx).concat(dots.slice(idx + 1, dots.length));
-  //     setDots(dots);
-  //   }
-  // };
-
   return (
     <div className="App">
-      {/* <PentatonicNameToShapeContainer /> */}
-      <PentatonicShapeToNameContainer />
+      <div class="sidenav">
+        <a href="#about">About</a>
+        <a href="#services">Services</a>
+        <a href="#clients">Clients</a>
+        <a href="#contact">Contact</a>
+      </div>{" "}
+      <div class="main">
+        <Routes>
+          <Route path="/" element={<PentatonicShapeToNameContainer />} />
+          <Route
+            path="/pentatonic-shape-to-name"
+            element={<PentatonicShapeToNameContainer />}
+          />
+          <Route
+            path="/pentatonic-name-to-shape"
+            element={<PentatonicNameToShapeContainer />}
+          />
+        </Routes>
+      </div>
     </div>
   );
 }
