4b8ccce59ab7efbeb978899a597ecfb3491c20f1
Bora Yalcin <b.yalcin@catawiki.nl>
2022-08-23 16:03:31 +0200

    updated to useRef for avoiding double init

--- src/FretboardComponent.js
+++ src/FretboardComponent.js
@@ -1,34 +1,39 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { Fretboard } from "@moonwave99/fretboard.js";
 import { noteFromPosition } from "./utils";
 import { config } from "./config";
 
-function FretboardComponent(props) {
-  const { showNotes, dots, editable, onClick } = props;
-  const isInitiated = useRef(false);
-
-  const [fretboard, setFretboard] = useState();
+function FretboardComponent({ showNotes, dots, editable, onClick }) {
+  const fretboard = useRef(null);
+  const fredboardEl = useRef(null);
 
   useEffect(() => {
-    if (!isInitiated.current) {
-      setFretboard(
-        new Fretboard({
-          el: "#fretboard",
-          fretCount: config.fretCount,
-          dotText: showNotes ? noteFromPosition : () => "",
-        })
-      );
-      isInitiated.current = true;
+    console.log("called on mount");
+
+    if (!fretboard.current && fredboardEl.current) {
+      console.log("called on mount: should work once");
+      fretboard.current = new Fretboard({
+        el: fredboardEl.current,
+        fretCount: config.fretCount,
+        dotText: showNotes ? noteFromPosition : () => "",
+      });
     }
+
+    return () => {
+      console.log("called on unmount");
+      fretboard.current.removeEventListeners();
+    };
   }, [showNotes, fretboard]);
+
   useEffect(() => {
-    if (fretboard) {
-      fretboard.clear();
-      fretboard.setDots(dots).render();
-      fretboard.on("click", editable ? onClick : () => null);
+    if (fretboard.current) {
+      fretboard.current.clear();
+      fretboard.current.setDots(dots).render();
+      fretboard.current.on("click", editable ? onClick : () => null);
     }
   }, [dots, onClick, editable, fretboard]);
-  return <figure id="fretboard"></figure>;
+
+  return <figure id="fretboard" ref={fredboardEl} />;
 }
 
 export { FretboardComponent };
